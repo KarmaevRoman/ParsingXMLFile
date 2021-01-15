bc41dc6168ea0307d930ba975adff4123c97ea2c
Кармаев Роман <karmaev05@gmail.com>
2021-01-15 13:33:20 +0700

    Создание главного класса команд

--- ParsingXMLFile/ViewModel/Base/ButtonCommand.cs
+++ ParsingXMLFile/ViewModel/Base/ButtonCommand.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
-namespace ParsingXMLFile.ViewModel
+namespace ParsingXMLFile.ViewModel.Base
 {
     internal abstract class ButtonCommand : ICommand
     {
