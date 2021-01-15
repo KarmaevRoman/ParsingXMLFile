67e9910ff5584bb5f2f47b28aa6bf3a085985d47
Кармаев Роман <karmaev05@gmail.com>
2021-01-15 12:02:58 +0700

    Добавление методов в класс с парсингом XML документа

--- ParsingXMLFile/Models/TreeElement.cs
+++ ParsingXMLFile/Models/TreeElement.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections.ObjectModel;
+using System.Xml;
 
 namespace ParsingXMLFile.Models
 {
@@ -12,5 +13,35 @@ namespace ParsingXMLFile.Models
         public string name { get; set; }
 
         public ObservableCollection<TreeElement> TreeElements { get; set; }
+
+        public void Add (TreeElement ChildElement)
+        {
+            TreeElements = new ObservableCollection<TreeElement>();
+            TreeElements.Add(ChildElement);
+        }
+        
+        public void AddTreeElementFromXml (XmlElement xmlElement)
+        {
+            TreeElements = new ObservableCollection<TreeElement>();
+            name = xmlElement.Name;                
+           
+            foreach (XmlAttribute SelectAtr in xmlElement.Attributes)
+                name += ($" {SelectAtr.Name} = '{SelectAtr.Value}'");
+
+            foreach (var ChildElemennt in xmlElement.ChildNodes)
+            {
+                if (ChildElemennt is XmlElement)
+                {                    
+                    var SubElement = new TreeElement();
+                    TreeElements.Add(SubElement);
+                    SubElement.AddTreeElementFromXml((XmlElement)ChildElemennt);
+                }
+
+                if (ChildElemennt is XmlText text)
+                {
+                    name += ($" - {text.Value}");
+                }
+            }
+        }
     }
 }
