e530ef89a09d7f55cadb2e2702c384d2dc80be1b
Кармаев Роман <karmaev05@gmail.com>
2021-01-15 12:03:34 +0700

    Удаление лишнего из файла инициализации окна

--- ParsingXMLFile/View/MainWindow.xaml.cs
+++ ParsingXMLFile/View/MainWindow.xaml.cs
@@ -12,17 +12,19 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ParsingXMLFile.Models;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Xml;
+
 
 namespace ParsingXMLFile
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
-            InitializeComponent();
-        }
+            InitializeComponent();            
+        }        
     }
 }
