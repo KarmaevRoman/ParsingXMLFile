2dea8c996a3d5a2ec6ba2d1a656c493d2862fbfe
Кармаев Роман <karmaev05@gmail.com>
2021-01-17 16:20:20 +0700

    Вынос метода загрузки файла

--- ParsingXMLFile/ViewModel/ViewModel.cs
+++ ParsingXMLFile/ViewModel/ViewModel.cs
@@ -36,35 +36,25 @@ namespace ParsingXMLFile.ViewModel
                 MessageBox.Show("Ссылка отсутствует.");
             else
             {
-                try
-                {
-                    xmlFile.Load(Link);
-                    xmlFile.Save("myfile.xml");
-                    MessageBox.Show("Файл успешно загружен.");                    
-                }
-
-                catch (Exception)
-                {
-                    MessageBox.Show("Неверная ссылка или некорректный XML файл.\nВведите еще раз.");
-                }
+                TryLoadXmlFile(xmlFile);
             }
         }
 
         public ICommand ParcingFile { get; }
         private bool CanParcingFileExecute (object p)
         {
-            return File.Exists("myfile.xml");
+            return File.Exists("temp_file.xml");
         }
 
         private void OnParcingFileExecute(object p)
         {
             var xmlFile = new XmlDocument();
             TreeElements = new ObservableCollection<TreeElement>();
-            xmlFile.Load("myfile.xml");
+            xmlFile.Load("temp_file.xml");
             var treeElement = new TreeElement();
             treeElement.AddTreeElementFromXml(xmlFile.DocumentElement);
             TreeElements.Add(treeElement);
-            File.Delete("myfile.xml");
+            File.Delete("temp_file.xml");
         }
 
         public ViewModel()
@@ -72,5 +62,20 @@ namespace ParsingXMLFile.ViewModel
             LoadXmlFile = new LambdaCommand(OnLoadXmlFileExecute, CanLoadXmlFileExecute);
             ParcingFile = new LambdaCommand(OnParcingFileExecute, CanParcingFileExecute);
         }
+
+        private void TryLoadXmlFile(XmlDocument xmlFile)
+        {
+            try
+            {
+                xmlFile.Load(Link);
+                xmlFile.Save("temp_file.xml");
+                MessageBox.Show("Файл успешно загружен.");
+            }
+
+            catch (Exception)
+            {
+                MessageBox.Show("Неверная ссылка или некорректный XML файл.\nВведите еще раз.");
+            }
+        }         
     }
 }
